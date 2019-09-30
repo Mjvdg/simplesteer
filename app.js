fc38331e7fa64cdd90a1708fa1d671fe026e32ab
Matthijs <matthijsmissiaen@hotmail.com>
2019-09-30 18:22:54 +0200

    before google maps

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
 "use strict";
-// require('./lib/gpsFuser').fuse();
 require('./lib/ArduinoApi')();
 const RtkClient = require('./lib/RtkClient');
 const State = require('./lib/state');
-// require('./lib/createLines');
 require('./lib/socketIO');
  
 State.eventEmitters.steering.on('turnRight', function(){
@@ -12,3 +10,4 @@ State.eventEmitters.steering.on('turnRight', function(){
 
 let rtkLeft = new RtkClient(0);
 let rtkRight = new RtkClient(1);
+
