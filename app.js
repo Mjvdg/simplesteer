3ba2244c820df1b88e6dbed1b4f825d54d06f8b1
Matthijs <matthijsmissiaen@hotmail.com>
2019-11-26 14:14:00 +0100

    cleanup

--- app.js
+++ app.js
@@ -1,14 +1,10 @@
 "use strict";
 require('./lib/GpsReceivers').setup();
 require('./lib/ArduinoApi').open();
-const RtkClient = require('./lib/RtkClient');
 const State = require('./lib/state');
 require('./lib/socketIO');
  
 State.eventEmitters.steering.on('turnRight', function(){
   console.log('turnRight');
 });
-
-let rtkLeft = new RtkClient(0);
-let rtkRight = new RtkClient(1);
 
